09e738b57ad81395ba4fd3d4f56ff87d1dfa2257
Thibaut Lartigue <lartiguethibaut@gmail.com>
2022-04-12 18:14:33 +0200

    Progress keep go one with timedowncounter and click event

--- js/game.js
+++ js/game.js
@@ -1,21 +1,53 @@
-
-
-
-
 class Game {
     constructor (){
         this.gridCol = 36;
         this.time = 0;
-        this.mines = 10;
+        this.mines = 5;
         this.playerScore = 0;
         this.shuffleArr = [];
+        this.cells = [];
     }
 
     
     start(){
         creatMatrix();
         
-        
+    }
+    
+
+
+    creatMatrix(){
+        const matrix = document.getElementById("matrix");
+            // let cells = [];
+            const shufflePositions = newGame.makingShufflingarr();  
+            for (let i = 0; i < this.gridCol; i ++){
+                let cell = document.createElement("div");
+                cell.setAttribute("id", i);
+                cell.classList.add(shufflePositions[i])               
+                matrix.appendChild(cell);
+                this.cells.push(cell); 
+                
+
+                cell.addEventListener('click', function(e){
+                  console.log(e.target);
+                  if (e.target.classList.contains('mines')){
+                      e.target.setAttribute("class", 'fallOnMine')
+                    
+                     console.log('You loose');
+                  } if (e.target.classList.contains('empty')){
+                      e.target.setAttribute("class", 'checked')
+                  }
+                //     if(e.target){
+                //       alert console.log(e.target);
+                //   }
+                })
+
+               
+            
+
+            }
+    
+            
     }
     
 
@@ -41,11 +73,40 @@ class Game {
     } 
 
 
+  
+
 }
 
 
 
+const newGame = new Game();
+newGame.creatMatrix();
 
-class PlayerScore{
 
+
+
+
+
+
+
+
+const startingMinutes = 2;
+let time = startingMinutes* 60;
+let refreshIntervalId = setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    if(seconds < 2){
+        seconds = '0' + seconds
+    } 
+    const countDownEl = document.getElementById("countDown");
+    countDownEl.innerHTML = `${minutes}:${seconds}`;
+
+    time--;
+
+    if (time < 0){
+        clearInterval(refreshIntervalId);
+    }
 }
